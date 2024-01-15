7e6487ee3befa2b4e3432e5eb486f0f2e17f04ae
morenabarboni <morena.barboni@unicam.it>
2024-01-15 18:31:55 +0100

    Update assetTransfer application

--- assetTransferApplication/index.js
+++ assetTransferApplication/index.js
@@ -3,63 +3,73 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
-
-
 const grpc = require('@grpc/grpc-js')
-const { connect, Contract, Identity, Signer, signers } = require('@hyperledger/fabric-gateway')
-const { Console } = require('console')
+const { connect, signers } = require('@hyperledger/fabric-gateway')
 const crypto = require('crypto')
 const fs = require('fs')
 const path = require('path')
 const { TextDecoder } = require('util')
 
-/**
- * envOrDefault() will return the value of an environment variable, or a default value if the variable is undefined.
- */
-function envOrDefault(key, defaultValue) {
-    return process.env[key] || defaultValue;
+// Path to crypto materials.
+const cryptoPath = '/workspaces/Fabric2.5_school_material/test-network/organizations/peerOrganizations/';
+
+// Gateway peer endpoint
+const peerEndpoints = {
+    'suppliera.quotation.com': 'localhost:7051',
+    'supplierb.quotation.com': 'localhost:9051',
+    'agency.quotation.com': 'localhost:11051'
 }
 
-const channelName = envOrDefault('CHANNEL_NAME', 'q1channel');
-const chaincodeName = envOrDefault('CHAINCODE_NAME', 'assetTransfer');
-const mspId = 'AgencyMSP';
-
-// Path to crypto materials.
-const cryptoPath = '/workspaces/Fabric2.5_school_material/test-network/organizations/peerOrganizations/agency.quotation.com/';
-
-// Path to user private key directory.
-const keyDirectoryPath = cryptoPath + 'users/User1@agency.quotation.com/msp/keystore'
-
-// Path to user certificate.
-const certPath = cryptoPath + '/users/User1@agency.quotation.com/msp/signcerts/User1@agency.quotation.com-cert.pem'
-
-// Path to peer tls certificate.
-const tlsCertPath = cryptoPath + 'peers/peer0.agency.quotation.com/tls/ca.crt';
-
-// Gateway peer endpoint.
-const peerEndpoint = 'localhost:11051'
-
-// Gateway peer SSL host name override.
-const peerHostAlias = 'peer0.agency.quotation.com'
+// mspIDs
+const orgMspIds = {
+    'suppliera.quotation.com': 'SupplierAMSP',
+    'supplierb.quotation.com': 'SupplierBMSP',
+    'agency.quotation.com': 'AgencyMSP'
+}
 
 const utf8Decoder = new TextDecoder();
-const assetId = `asset${Date.now()}`;
 
-async function newGrpcConnection() {
+/**
+ * Establish client-gateway gRPC connection
+ * @param {String} organization | organization domain
+ * @returns gRPC client
+ */
+async function newGrpcConnection(organization) {
+    // Gateway peer SSL host name override.
+    const peerHostAlias = `peer0.${organization}`
+    // Path to peer tls certificate.
+    const tlsCertPath = path.join(cryptoPath, `${organization}/peers/${peerHostAlias}/tls/ca.crt`)
+
     const tlsRootCert = fs.readFileSync(tlsCertPath);
     const tlsCredentials = grpc.credentials.createSsl(tlsRootCert);
-    return new grpc.Client(peerEndpoint, tlsCredentials, {
+
+    return new grpc.Client(peerEndpoints[organization], tlsCredentials, {
         'grpc.ssl_target_name_override': peerHostAlias,
     });
 }
 
-function newIdentity() {
-    const credentials = fs.readFileSync(certPath)
-    return { mspId, credentials}
+/**
+ * Create a new user identity
+ * @param {String} organization | organization domain
+ * @returns the user credentials
+ */
+function newIdentity(organization) {
+    // Path to user certificate
+    const certPath = path.join(cryptoPath, `${organization}/users/User1@${organization}/msp/signcerts/User1@${organization}-cert.pem`)
+    const mspId = orgMspIds[organization];
+    const credentials = fs.readFileSync(certPath);
+    return { mspId, credentials }
 }
 
-function newSigner() {
+/**
+ * Create a signing implementation
+  * @param {String} organization | organization domain
+  * @returns a new signing implementation for the user
+ */
+function newSigner(organization) {
+    // Path to user private key directory.
+    const keyDirectoryPath = path.join(cryptoPath, `${organization}/users/User1@${organization}/msp/keystore`)
+
     const files = fs.readdirSync(keyDirectoryPath)
     const keyPath = path.resolve(keyDirectoryPath, files[0])
     const privateKeyPem = fs.readFileSync(keyPath)
@@ -69,16 +79,28 @@ function newSigner() {
 
 /**
  * Submit a transaction synchronously, blocking until it has been committed to the ledger.
+  * @param {String} organization | organization domain
+  * @param {String} channel | channel name
+  * @param {String} chaincode | chaincode name 
+  * @param {String} transactionName | transaction method
+  * @param {Array} transactionParams | transaction parameters
+  * @returns a new signing implementation for the user
  */
-async function submitT(channel, transactionName, transactionParams) {
-    console.log('\n--> Submit Transaction: CreateAsset, creates new asset with ID, Color, Size, Owner and AppraisedValue arguments');
+async function submitT(organization, channel, chaincode, transactionName, transactionParams) {
 
-    const client = await newGrpcConnection()
-    console.log("gRPC Connection created")
+    organization = organization.toLowerCase()
 
-    const id = newIdentity()
-    const signer = newSigner()
+    //Establish gRPC connection
+    console.log("\nCreating gRPC connection...")
+    const client = await newGrpcConnection(organization)
 
+    console.log(`Retrieving identity for User1 of ${organization} ...`)
+    //Retrieve User1's identity
+    const id = newIdentity(organization)
+    //Retrieve signing implementation
+    const signer = newSigner(organization)
+
+    //Connect gateway
     const gateway = connect({
         client,
         identity: id,
@@ -99,10 +121,13 @@ async function submitT(channel, transactionName, transactionParams) {
     })
 
     try {
-        console.log("Connecting to the channel...")
+        console.log(`Connecting to ${channel} ...`)
         const network = gateway.getNetwork(channel)
-        console.log("Getting the contract...")
-        const contract = network.getContract('assetTransfer')
+
+        console.log(`Getting the ${chaincode} contract ...`)
+        const contract = network.getContract(chaincode)
+
+        console.log(`Submitting ${transactionName} transaction ...\n`)
 
         let resp = null
         if (!transactionParams || transactionParams === '') {
@@ -110,9 +135,28 @@ async function submitT(channel, transactionName, transactionParams) {
         } else {
             resp = await contract.submitTransaction(transactionName, ...transactionParams)
         }
+        const resultJson = utf8Decoder.decode(resp);
 
-        console.log(resp.toString())
+        if (resultJson && resultJson !== null) {
+            const result = JSON.parse(resultJson);
+            console.log('*** Result:', result);
+        }
+        console.log('*** Transaction committed successfully');
 
+        const events = await network.getChaincodeEvents(chaincode, { startBlock: BigInt(0) });
+        try {
+            for await (const event of events) {
+                const asset = new TextDecoder().decode(event.payload);
+
+                console.log(`*** Contract Event Received: ${event.eventName}`)
+                console.log(`-- asset: ${asset}`)
+                console.log(`-- chaincodeName: ${event.chaincodeName}`)
+                console.log(`-- transactionId: ${event.transactionId}`)
+                console.log(`-- blockNumber: ${event.blockNumber}\n`)
+            }
+        } finally {
+            events.close();
+        }
     } catch (err) {
         console.error(err)
     } finally {
@@ -120,7 +164,6 @@ async function submitT(channel, transactionName, transactionParams) {
         client.close()
     }
 
-    console.log('*** Transaction committed successfully');
 }
 
 module.exports = { submitT }
